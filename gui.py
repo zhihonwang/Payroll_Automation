e9f7515fdbbb48381c6e044c0c88bc18cd97034a
ZeehomWang <zhihonwang@outlook.com>
2023-07-11 23:33:34 +0800

    bug fixed
    更新时间戳+日志

--- gui.py
+++ gui.py
@@ -5,6 +5,8 @@ from IPython.display import HTML
 import smtplib  # 发邮件
 from email.mime.text import MIMEText  # 用于构建内容文本
 from email.header import Header  # 用于构建邮件头
+from datetime import datetime # 获取时间
+
 # 备注信息
 text = psg.popup_get_text("请输入邮件内容：")
 if not text:
@@ -36,9 +38,12 @@ else:
 
             df = pd.DataFrame(forms1)
             # 获取时间
-
+            dt01 = datetime.today()
+            # print(dt01.date()) # 获取日期
+            # print(dt01.time()) # 获取时间
+            month = dt01.month-1 # 获取发薪月
             # 邮件尾部署名
-            footer = (f'芜湖海立新能源-经营管理部 2023年7月10日')
+            footer = f'<br>芜湖海立新能源-经营管理部 <br>{dt01.date()}'
             # 工资明细主体
             html = df.to_html(header=False)
             # 将h5生成到文件
@@ -54,13 +59,13 @@ else:
             # 对方账户
             to_addr = [employee_email]  # 添加多个账户采用列表形式
             # 要发送的内容
-            moment = (f"Hi {employee_name} 您好！ 以下为 {salary_month} 月工资明细，请查收~如有疑问请联系经营管理部[董敏]。br<>\n" + text + html + footer)
+            moment = (f"Hi {employee_name} 您好！<br>&emsp;&emsp;以下为您 {month} 月工资明细，请查收~ 如有疑问请联系经营管理部-[董敏]。<br>" + f'备注: {text}' + html + footer)
             # 构建纯文本的邮件内容
             msg = MIMEText(moment, 'html', 'utf-8')
             # 构建邮件头
             msg['From'] = Header('payroll@highly-whnet.com')  # 发件人的名称或地址
             msg['To'] = Header(employee_email)  # to收件人邮箱地址
-            msg['Subject'] = Header(f'{employee_name}<{salary_month}>月工资表')  # 主题
+            msg['Subject'] = Header(f'{employee_name} {month} 月工资明细')  # 主题
 
             server = smtplib.SMTP_SSL(host)  # 开启发信服务
             server.connect(host, port)  # 连接发信服务
@@ -70,6 +75,8 @@ else:
             print(f'[{employee_name}]{salary_month}月的薪资信息已发送至{mail_addr}')
             # psg.popup_notify(f'[{employee_name}]{salary_month}月的薪资信息已发送至{mail_addr}')
             with open('./log.txt', 'a', encoding='UTF-8') as f:
-                f.write(f'[{employee_name}]{salary_month}月的薪资信息已发送至{mail_addr}')
+                f.write(f'{dt01.date()} + {dt01.time()}: [{employee_name}]{month}月的薪资信息已发送至{mail_addr}\n')
         except Exception as e:
             print(e)
+
+psg.popup_notify(f'{row_count - 1}条数据均已发送，发送日志请查看log.txt文件')
